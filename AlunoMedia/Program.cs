9eae2c389fe6a1352a4dc0c4bfa6ed783de14415
masfazan <msfazan@icloud.com>
2024-04-18 21:00:42 -0300

    Add project files.

---
+++ AlunoMedia/Program.cs
@@ -0,0 +1,24 @@
+﻿//float nota1, nota2, media;
+
+Console.WriteLine("Digite a nota: ");
+float nota1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota: ");
+float nota2 = float.Parse(Console.ReadLine());
+
+float media = (nota1 + nota2) / 2;
+
+if (media <= 3)
+{
+    Console.WriteLine("Reprovado");
+}
+else
+if (media < 5)
+{
+    Console.WriteLine("Exame");
+}
+else
+{
+    Console.WriteLine("Aprovado");
+}
+Console.ReadLine();
